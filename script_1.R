be12cc088d4bdb7558b3485806a8bee6892ce848
juliabirnie <juliabirnie@live.com>
2023-08-29 16:45:03 -0300

    Testando segundo commit

---
+++ script_1.R
@@ -0,0 +1,19 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+#+++++++++++++++++++++ Análise Exploratória de Dados ++++++++++++++++++++++++++#
+#+++++++++++++++++++++++++ Júlia Birnie Farias ++++++++++++++++++++++++++++++++#
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+## 1) Informações sobre o projeto:
+
+# Script para AED de um banco de dados do kaggle 
+#(Heart Attack Analysis & Prediction Dataset)
+
+## 2) Carregando bibliotecas:
+
+library(tableone)
+library(ggplot2)
+
+## 3) Carregando base de dados:
+dados <- read.csv
+("C:/Users/Windows/Desktop/JULIA/Github/julia_aed_1/Dados/heart.csv")
+
